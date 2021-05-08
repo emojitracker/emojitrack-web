45d0ac4dfe4ae337d71b3bfc013deb48f2c4bf06
Matthew Rothenberg <mrothenberg@gmail.com>
2021-05-08 01:42:46 -0400

    feat: remove deprecated graphite logging

--- lib/config.rb
+++ lib/config.rb
@@ -1,6 +1,3 @@
-require 'uri'
-require 'socket'
-
 #convenience method for reading booleans from env vars
 def to_boolean(s)
   s and !!s.match(/^(true|t|yes|y|1)$/i)
@@ -20,21 +17,4 @@ end
 
 def is_development?
   ENV["RACK_ENV"] == 'development'
-end
-
-# configure logging to graphite in production
-def graphite_log(metric, count)
-  @hostedgraphite_apikey = ENV['HOSTEDGRAPHITE_APIKEY']
-  # puts "Graphite log - #{metric}: #{count}" if VERBOSE
-  if is_production?
-    sock = UDPSocket.new
-    sock.send @hostedgraphite_apikey + ".#{metric} #{count}\n", 0, "carbon.hostedgraphite.com", 2003
-  end
-end
-
-# same as above but include heroku dyno hostname
-def graphite_dyno_log(metric,count)
-  dyno = ENV['DYNO'] || 'unknown-host'
-  metric_name = "#{dyno}.#{metric}"
-  graphite_log metric_name, count
 end
